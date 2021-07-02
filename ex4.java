ffb3c4a41346e0e9b704130bfc33cdf2547a962b
s1280214 <86853804+s1280214@users.noreply.github.com>
2021-07-03 04:49:04 +0900

    Initial revision

---
+++ ex4.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+import java.lang.Math;
+  
+public class Main {
+      public static void main(String[] args) {
+  
+System.out.println("Rolling the dice...");
+  
+          int goal;
+  
+        int val1 = (int) (Math.random() * 6)+1;
+        System.out.println("Die 1:"+val1);
+
+        int val2 = (int) (Math.random() * 6)+1;
+        System.out.println("Die 2:"+val2);
+        goal = val1 + val2;
+
+        System.out.println("Total value : "+goal);
+}
+}
